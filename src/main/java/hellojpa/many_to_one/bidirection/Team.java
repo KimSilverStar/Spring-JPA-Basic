478bf8921fdda8df18054a45e946c00693e30f34
Kim EunSung <dmstjd365@naver.com>
2022-02-24 16:24:08 +0900

    다대일(N:1) 연관관계 매핑
    다대일 단방향
    - @JoinColumn 으로 외래 키 지정
    
    다대일 양방향
    - 다대일, 단방향에서 반대편(대상 테이블)에 참조 추가
      => 단방향에서 반대편에 참조 추가해도, 테이블에는 변화 X
    - @JoinColumn 으로 외래 키 지정
    - 외래 키가 있는 쪽이 양방향 연관관계의 주인
      => 반대편(Team)에 mappedBy 로 양방향 연관관계의 주인 명시

---
+++ src/main/java/hellojpa/many_to_one/bidirection/Team.java
@@ -0,0 +1,36 @@
+package hellojpa.many_to_one.bidirection;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+	@Id @GeneratedValue
+	@Column(name = "TEAM_ID")
+	private Long id;
+
+	@OneToMany(mappedBy = "team")		// 양방향 연관관계의 주인: Member.team
+	private List<Member> members = new ArrayList<>();
+
+	@Column(name = "NAME")
+	private String name;
+
+	public Team() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
