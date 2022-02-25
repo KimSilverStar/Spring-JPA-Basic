ab350e128448b6df93bc83dea096805ee6406a4c
Kim EunSung <dmstjd365@naver.com>
2022-02-25 20:02:37 +0900

    컬렉션 값 타입
    - `Set<String> favoriteFoods`, `List<Address>`
    - DB는 컬렉션을 같은 테이블에 저장이 불가능
      => 컬렉션을 저장할 별도의 테이블: FAVORITE_FOOD, ADDRESS

---
+++ src/main/java/hellojpa/value_type/collection_value_type/JpaMain.java
@@ -0,0 +1,89 @@
+package hellojpa.value_type.collection_value_type;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Set;
+
+public class JpaMain {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		// persistence.xml 의 <persistence-unit name="hello">
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();		// DB 커넥션 얻기
+		tx.begin();
+
+		try {
+			Member member = new Member();
+			member.setUsername("member1");
+			member.setHomeAddress(
+					new Address("homeCity", "street", "100")
+			);
+
+			member.getFavoriteFoods().add("치킨");		// 컬렉션 값 타입에 원소 추가
+			member.getFavoriteFoods().add("피자");
+			member.getFavoriteFoods().add("족발");
+
+			member.getAddressHistory().add(
+					new Address("old1", "street1", "1")
+			);
+			member.getAddressHistory().add(
+					new Address("old2", "street2", "2")
+			);
+
+			em.persist(member);
+			// 1) MEMBER 테이블에 member 값 추가
+			// 2) FAVORITE_FOOD 테이블에 원소 3개 추가
+			// 3) ADDRESS 테이블에 원소 2개 추가
+			// 값 타입(기본 값, 임베디드, 컬렉션)의 라이프 사이클
+			// => 해당 값을 소유한 엔티티에 의존
+
+			em.flush();
+			em.clear();
+
+			System.out.println("--------------- START ---------------");
+			Member findMember = em.find(Member.class, member.getId());
+
+			// 컬렉션 값 타입도 디폴트로 지연 로딩 전략
+			List<Address> addressHistory = findMember.getAddressHistory();
+			for (Address address : addressHistory)
+				System.out.println("address = " + address.getCity());
+
+			Set<String> favoriteFoods = findMember.getFavoriteFoods();
+			for (String favoriteFood : favoriteFoods)
+				System.out.println("favoriteFood = " + favoriteFood);
+
+			// 값 타입의 수정 - setter 수정자로 수정하지 말고, 새로운 값을 채운 객체를 새로 생성
+			// => setter 를 통한 수정을 막기 위해, setter 를 정의 X 한 불변 객체 이용할 것
+			Address oldAddress = findMember.getHomeAddress();
+			findMember.setHomeAddress(
+					new Address("newCity", oldAddress.getStreet(), oldAddress.getZipcode())
+			);
+
+			// 치킨 -> 한식 으로 교체
+			findMember.getFavoriteFoods().remove("치킨");
+			findMember.getFavoriteFoods().add("한식");
+
+			// 자바 컬렉션의 remove(): 기본적으로 해당 원소를 찾을 때, equals() 메소드 이용
+			// => 해당 타입의 equals(), hashCode() 메소드 재정의 필요
+			findMember.getAddressHistory().remove(
+					new Address("old1", "street1", "1")
+			);
+			findMember.getAddressHistory().add(
+					new Address("newCity", "street", "100")
+			);
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		} finally {
+			em.close();
+		}
+
+		emf.close();
+	}
+}
---
+++ src/main/java/hellojpa/value_type/collection_value_type/Member.java
@@ -0,0 +1,84 @@
+package hellojpa.value_type.collection_value_type;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Member {
+	@Id @GeneratedValue
+	@Column(name = "MEMBER_ID")
+	private Long id;
+
+	@Column(name = "USERNAME")
+	private String username;
+
+	@Embedded		// 임베디드 타입 명시
+	private Period workPeriod;
+
+	@Embedded		// 임베디드 타입 명시
+	private Address homeAddress;
+
+	@ElementCollection		// 컬렉션 값 타입 명시
+	@CollectionTable(name = "FAVORITE_FOOD",
+			joinColumns = @JoinColumn(name = "MEMBER_ID"))		// 컬렉션이 저장될 테이블 명시
+	@Column(name = "FOOD_NAME")
+	private Set<String> favoriteFoods = new HashSet<>();
+
+	@ElementCollection		// 컬렉션 값 타입 명시
+	@CollectionTable(name = "ADDRESS",
+			joinColumns = @JoinColumn(name = "MEMBER_ID"))		// 컬렉션이 저장될 테이블 명시
+	private List<Address> addressHistory = new ArrayList<>();
+
+	public Member() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Period getWorkPeriod() {
+		return workPeriod;
+	}
+
+	public void setWorkPeriod(Period workPeriod) {
+		this.workPeriod = workPeriod;
+	}
+
+	public Address getHomeAddress() {
+		return homeAddress;
+	}
+
+	public void setHomeAddress(Address homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+
+	public Set<String> getFavoriteFoods() {
+		return favoriteFoods;
+	}
+
+	public void setFavoriteFoods(Set<String> favoriteFoods) {
+		this.favoriteFoods = favoriteFoods;
+	}
+
+	public List<Address> getAddressHistory() {
+		return addressHistory;
+	}
+
+	public void setAddressHistory(List<Address> addressHistory) {
+		this.addressHistory = addressHistory;
+	}
+}
